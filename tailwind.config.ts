ed2331f09e518f630d14daacc018cdf32587a643
Andrea Cazzola <andrea.cazzola@getconnected.it>
2023-08-26 19:14:00 +0200

    add configurations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,11 +12,33 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        sans: ["ui-sans-serif"],
+        serif: ["ui-serif"],
+        mono: ["ui-monospace"],
+      },
     },
   },
   daisyui: {
-    themes: ["light"],
+    themes: [
+      {
+        mytheme: {
+          primary: "#65c3c8",
+          secondary: "#ef9fbc",
+          accent: "#eeaf3a",
+          neutral: "#291334",
+          "base-100": "#faf7f5",
+          info: "#3abff8",
+          success: "#36d399",
+          warning: "#fbbd23",
+          error: "#f87272",
+        },
+      },
+    ],
   },
-  plugins: [require("daisyui")],
+  corePlugins: {
+    aspectRatio: false,
+  },
+  plugins: [require("daisyui"), require("@tailwindcss/aspect-ratio")],
 };
 export default config;
